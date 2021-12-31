7fe11ed70ce37759891300a52d5bde20f51f4a97
Tom Zhang <tianze.zhang@gmail.com>
2021-12-31 13:56:07 +0000

    implemented line chart with d3 but cannot load data to chart

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const { Octokit } = require('@octokit/core');
+const fs = require('fs');
+const csv = require('csv-parser');
 const octokit = new Octokit({
-    auth: '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',
+    auth: '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',
 });
 
 
 const owner = 'tomztz';
-const repo = 'Java-Projects';
+const repo = 'computer-vision-model';
 const url =  '/repos/{owner}/{repo}/{path}'; 
 const ref =  'heads/master'; 
 
@@ -26,9 +28,9 @@ const getContents = async () => {
 
 const getCommits = async () => {
     
-    const { data } = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-        owner: 'tomztz',
-        repo: 'Java-Projects'
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}/commits?per_page=100', {
+        owner,
+        repo
       });
       
       return data;
@@ -43,25 +45,61 @@ const getTotalCommits = async () =>{
       shaCount++ 
   }
 });
-    console.log(shaCount);
+    
     return shaCount;
 }
 
-/* GET users listing. */
-router.get('/', async(req, res, next) => {
-    
-  
-    result = await getTotalCommits(); 
-    //res.send("Total commits are "+result);
-    res.setHeader('Content-Type', 'application/json');
-    res.json({commits: result});
-   
-    //res.send(JSON.stringify(result));
-  
-  
-  
-  
+const getCommitsByDate = async(startDate,endDate) =>{
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}/commits?per_page=100&since=${startDate}&until=${endDate}', {
+        owner,
+        repo,
+        startDate,
+        endDate
+      });
+      return data;
+}
 
+const getTotalCommitsByDate = async(startDate,endDate) =>{
+    var data = await getCommitsByDate(startDate,endDate);
+    var count = 0;
+    arr = JSON.parse(JSON.stringify(data));//json
+    arr.map(function(val){
+      if(val.sha) {
+      count++ 
+  }
+});
+    
+    return count;
+}
+/* GET users listing. */
+router.get('/', async(req, res, next) => {  
+    
+    //var result = await getTotalCommits(); 
+    var result1 = await getTotalCommitsByDate('2021-04-01T00:00:00Z','2021-04-05T23:59:59Z');
+    var result2 = await getTotalCommitsByDate('2021-04-05T00:00:00Z','2021-04-10T23:59:59Z');
+    var result3 = await getTotalCommitsByDate('2021-04-10T00:00:00Z','2021-04-15T23:59:59Z');
+    var result4 = await getTotalCommitsByDate('2021-04-15T00:00:00Z','2021-04-20T23:59:59Z');
+    var result5 = await getTotalCommitsByDate('2021-04-20T00:00:00Z','2021-04-25T23:59:59Z');
+    
+    //res.send("Total commits are "+result);
+    var arr = [{//totalCommits : result}
+        date:'01/04/2021',value: result1},
+       {date:'05/04/2021',value : result2},
+       {date:'10/04/2021',value : result3},
+       {date:'15/04/2021',value : result4},
+       {date:'20/04/2021',value : result5}
+       ]; 
+       
+    res.setHeader('Content-Type', 'application/json');
+    res.json(arr);
+    
+    var csv = "Date,Commits\r\n"+arr[0].date+","+arr[0].value+"\r\n"+arr[1].date+","+arr[1].value
+    +"\r\n"+arr[2].date+","+arr[2].value+"\r\n"+arr[3].date+","+arr[3].value+
+    "\r\n"+arr[4].date+","+arr[4].value;
+
+
+    fs.writeFileSync("data.csv", csv);
+    //res.send(JSON.stringify(result));
     
 });
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,116 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { scaleLinear, scaleBand } from 'd3-scale';
+import XYAxis from './components/axis/xy-axis';
+import Line from './components/line/line';
+import { line, curveMonotoneX } from 'd3-shape';
+import { extent } from 'd3-array';
+import { transition } from 'd3-transition';
+
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { apiResponse: "" };
-    }
+    
+    constructor() {
+        super();
+        this.state = {users: []}
+        
+        /*this.state= {data: [
+            { name: "01/04/2021", value: 30 },
+            { name: "", value: 10 },
+            { name: 'Mar', value: 50 },
+            { name: 'Apr', value: 20 },
+            { name: 'May', value: 80 },
+            { name: 'Jun', value: 30 },
+            { name: 'July', value: 0 },
 
+          ],
+     
+        }*/
+        
+      }
+    /*
     callAPI() {
          fetch("http://localhost:3001/users")
-            .then(res => res.text())
+            .then(res => JSON.parse(res))
             .then(res => this.setState({ apiResponse: res }))
             .catch(err => err);
-    }
+    }*/
 
     componentDidMount() {
-        this.callAPI();
-    }
+    fetch("http://localhost:3001/users")
+      .then( res => {
+        console.log(res);
+        return res.json()
+     })   
+     .then(users => { 
+        console.log(users); 
+        this.setState({ users })
+     });
+  }
 
     render() {
+        
+        
+        const data =   
+            [
+            { name: "01/04/2021", value: 1 },
+            { name: "", value: 10 },
+            { name: "", value: 50 },
+            { name: "  ", value: 20 },
+            { name: 'May', value: 80 }
+
+          ]
+    const parentWidth = 500;
+
+    const margins = {
+      top: 20,
+      right: 20,
+      bottom: 100,
+      left: 20,
+    };
+
+    const width = parentWidth - margins.left - margins.right;
+    const height = 200 - margins.top - margins.bottom;
+
+    const ticks = 5;
+    const t = transition().duration(1000);
+
+    const xScale = scaleBand()
+      .domain(data.map(d => d.name))
+      .rangeRound([0, width]).padding(0.1);
+
+    const yScale = scaleLinear()
+      .domain(extent(data, d => d.value))
+      .range([height, 0])
+      .nice();
+
+    const lineGenerator = line()
+      .x(d => xScale(d.name))
+      .y(d => yScale(d.value))
+      .curve(curveMonotoneX);
         return (
             <div className="App">
+                
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
+                <svg
+                  className="lineChartSvg"
+                  width={width + margins.left + margins.right}
+                  height={height + margins.top + margins.bottom}
+                >
+                  <g transform={`translate(${margins.left}, ${margins.top})`}>
+                    <XYAxis {...{ xScale, yScale, height, ticks, t }} />
+                    <Line data={data} xScale={xScale} yScale={yScale} lineGenerator={lineGenerator} width={width} height={height} />
+                  </g>
+                </svg>
+                {this.state.users.map(user =>
+                <div key={user.id}>name: {user.name} value: {user.value}</div>
+              )}
             </div>
+            
         );
     }
 }
